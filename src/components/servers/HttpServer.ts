e95fdb8736ca62775f9bf76e1fbef0abce018f4d
Néstor Daza <git@extensa.io>
2024-05-15 16:52:24 -0400

    adding thanks page

--- src/components/servers/HttpServer.ts
+++ src/components/servers/HttpServer.ts
@@ -14,6 +14,9 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
 const service = new TurnService();
 
 const port = process.env.PORT || '8000';
@@ -24,7 +27,7 @@ io.on('connection', (socket) => {
   io.emit(MessageType.TURN_URL, `${service.nextAvailableTurn}`);
 });
 
-app.post('/getTurn/:id', (req, res) => {
+app.post('/getTurn/:id', async (req, res) => {
   const id = req.params.id;
   const userName = req.body && req.body.user_name ? req.body.user_name : 'anonymous';
   console.log(userName);
@@ -32,11 +35,13 @@ app.post('/getTurn/:id', (req, res) => {
 
   const assignedTurn = assignAndEmit(id, userName);
 
-  res.send(`<h1>${assignedTurn}</h1>`);
+  res.redirect(`/thanks.html?name=${userName}&turn=${assignedTurn}`);
 });
 
 app.get('/getTurn/:id', (req, res) => {
-  const assignedTurn = assignAndEmit(req.params.id, 'anonymous');
+  const userName = 'anonymous';
+  const assignedTurn = assignAndEmit(req.params.id, userName);
+
   res.send(`<h1>${assignedTurn}</h1>`);
 });
 
